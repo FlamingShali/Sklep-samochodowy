a43fede84cbf4bf12fdcfa283fa2d372b91ecf7f
Shaliryo1567 <michal.klepner1@o2.pl>
2024-04-17 12:09:27 +0200

    1st update

--- mainScript.js
+++ mainScript.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const car = document.getElementById('car')
+const details = document.querySelector('.details') 
+const mainPage = document.querySelector('.mainPage')
+const accessories = document.querySelector('.accessories')
+
+
+
+//Aktualizowanie elementu html
+//
+const showCarDetails = function(){
+    details.classList.remove('hidden')
+    console.log('cos')
+}
+
+const nextPage = function(){
+    details.classList.add('hidden')
+    accessories.classList.remove('hidden')
+}
+
+
+
+
